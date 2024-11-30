378c78e2529513eb7e307f965f54096db47cad34
Clement Hugo <118815063+hugoclmt@users.noreply.github.com>
2024-11-30 18:33:46 +0100

    Création de la logique pour l ig

--- app/src/main/java/com/example/todolist/SecondMainActivity.kt
+++ app/src/main/java/com/example/todolist/SecondMainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.todolist
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import entity.Task
+import entity.TaskList
 import entity.User
 
 class SecondMainActivity: AppCompatActivity() {
@@ -24,10 +31,51 @@ class SecondMainActivity: AppCompatActivity() {
         val nomUtilisateur = intent.getStringExtra("nomUtilisateur")
 
         val user = User(1,nomUtilisateur.toString())
-
+        val taskList = TaskList()
         val txtViewNomUtilisateur = findViewById<TextView>(R.id.nomUtilisateurTodoList)
 
         txtViewNomUtilisateur.text = "Bienvenue ${user.name}"
+
+        val spinner: Spinner = findViewById(R.id.spinner_priority)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.priority_levels,
+            android.R.layout.simple_spinner_item
+        )
+        var idTask = 0
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = adapter
+
+        val textTask = findViewById<EditText>(R.id.text_task)
+
+        val btnaddTask = findViewById<Button>(R.id.add_task)
+
+        var tasks = taskList.getListOfTask()
+
+
+
+
+
+
+
+        btnaddTask.setOnClickListener {
+            val textTask = textTask.text.toString()
+            val selectedTaskPriority = spinner.selectedItem.toString().toInt()
+            if (textTask.isNotBlank() && (selectedTaskPriority in 1..5)) {
+                idTask++
+                val task = Task(idTask,textTask,selectedTaskPriority)
+                taskList.addTask(task)
+                tasks.forEach { task ->
+                    val taskDetails = "ID: ${task.id}, Description: ${task.description}, Niveau: ${task.level}, Fait: ${task.isDone}"
+                    Toast.makeText(this, taskDetails, Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Veuillez remplir tout les champs",Toast.LENGTH_SHORT).show()
+            }
+        }
+
 
     }
 
--- app/src/main/java/entity/Task.kt
+++ app/src/main/java/entity/Task.kt
@@ -1,5 +1,5 @@
 package entity
-data class Task (val id: Long, val description : String, val level : Int, var isDone : Boolean = false)
+data class Task (val id: Int, val description : String, val level : Int, var isDone : Boolean = false)
 {
     fun toggleDone(){
         this.isDone = !this.isDone
